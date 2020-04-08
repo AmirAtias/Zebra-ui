d71adee33047f7cb073d39cebe57aeecc01101d3
Amir Atias <amiratias505@gmail.com>
2020-04-08 11:43:36 +0300

    add spinner to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,23 @@
 import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { css } from "@emotion/core";
+import API from "../utils/API";
+import { Spinner } from "react-bootstrap";
+
 const NavBar = () => {
+  const [handling, setHandling] = useState(false);
+  useEffect(() => {
+    const interval = setInterval(async () => {
+      try {
+        const response = await API.get("/socialMedia/requestStatus");
+        console.log("👉 Returned data:", response.data);
+        setHandling(response.data.handleRequest);
+      } catch (e) {
+        window.alert(`😱 Axios request failed: ${e}`);
+      }
+      return () => clearInterval(interval);
+    }, 15000);
+  }, []);
   const navCss = css({
     display: "flex",
     flexFlow: "column wrap",
@@ -12,9 +28,13 @@ const NavBar = () => {
     <div css={navCss}>
       <Navbar bg="light" variant="light">
         <Container>
-          <Navbar.Brand href="/">Home</Navbar.Brand>
+          <Navbar.Brand href="/">
+            <i style={{ paddingRight: "5%" }} className="fas fa-home"></i>
+            Home
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Nav>
+            {handling && <Spinner animation="border" variant="primary" />}
             <NavDropdown title="User Mangement" id="nav-dropdown">
               <NavDropdown.Item href="/CrawlingRequest">
                 Crawling Request
